49b6b31d56bb2415a636be0e9df3b979808242b4
Timon Langlotz <timon.langlotz@gmail.com>
2015-05-17 23:15:00 +0200

    add first commands to script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Right directory?
+echo -n "Are you in the directory of the external harddrive? [y|n] "
+read res
+if [[ $res =~ ^[Nn]$ ]] then
+	exit
+fi
+
+# Install Node.js
+curl -sLS https://apt.adafruit.com/add | sudo bash &&
+sudo apt-get install node npm &&
+npm install pm2 -g &&
+
+# Clone ka-lite and index-server
+git clone https://github.com/learningequality/ka-lite.git &&
+git clone https://github.com/FabulinusFoundation/fabulinus-index-server.git &&
